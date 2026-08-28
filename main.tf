e20c47d2c389530d50da98f36224cf8cbe92ec61
Abhimanyu Gupta <aarushgupta888@gmail.com>
2026-08-28 22:29:21 +0530

    Add Terraform configuration

---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=5.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "demo" {
+  name     = "rg-devops-practice"
+  location = "West Europe"
+}
